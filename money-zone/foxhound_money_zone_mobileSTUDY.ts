95358040bec1c7114d0671f658335676a402519c
bigboss-trades <92008226+bigboss-trades@users.noreply.github.com>
2022-04-12 09:34:41 -0800

    Update foxhound_money_zone_mobileSTUDY.ts
    Added option to choose time per profile.

--- money-zone/foxhound_money_zone_mobileSTUDY.ts
+++ money-zone/foxhound_money_zone_mobileSTUDY.ts
@@ -5,16 +5,48 @@
 #
 # RECOMMENDED STUDY NAME: foxhound_money_zone_mobile
 #
+# VERSION 1.2 - ADDED TIME PER PROFILE (thanks @dstar!)
 # VERSION 1.1 - ADDED VOLUME PROFILE POC
 #               Changed method to color bars to be mobile friendly
 # VERSION 1.0 - INITIAL RELEASE
 
 declare once_per_bar;
 
-def yyyymmdd = GetYYYYMMDD();
-def period = CountTradingDays(Min(First(yyyymmdd), yyyymmdd), yyyymmdd) - 1;
-def count = CompoundValue(1, if period != period[1] then (count[1] + period - period[1]) % 1 else count[1], 0);
+input timePerProfile = { CHART, MINUTE, HOUR, default DAY, WEEK, MONTH, "OPT EXP", BAR};
+def multiplier = 1.0;
+
+def period;
+def yyyymmdd = getYyyyMmDd();
+def seconds = secondsFromTime(0);
+def month = getYear() * 12 + getMonth();
+def day_number = daysFromDate(first(yyyymmdd)) + getDayOfWeek(first(yyyymmdd));
+def dom = getDayOfMonth(yyyymmdd);
+def dow = getDayOfWeek(yyyymmdd - dom + 1);
+def expthismonth = (if dow > 5 then 27 else 20) - dow;
+def exp_opt = month + (dom > expthismonth);
+switch (timePerProfile) {
+case CHART:
+    period = 0;
+case MINUTE:
+    period = floor(seconds / 60 + day_number * 24 * 60);
+case HOUR:
+    period = floor(seconds / 3600 + day_number * 24);
+case DAY:
+    period = countTradingDays(Min(first(yyyymmdd), yyyymmdd), yyyymmdd) - 1;
+case WEEK:
+    period = floor(day_number / 7);
+case MONTH:
+    period = floor(month - first(month));
+case "OPT EXP":
+    period = exp_opt - first(exp_opt);
+case BAR:
+    period = barNumber() - 1;
+}
+
+
+def count = CompoundValue(1, if period != period[1] then (count[1] + period - period[1]) % multiplier else count[1], 0);
 def cond = count < count[1] + period - period[1];
+
 
 profile mpro = TimeProfile(onexpansion = no, startNewProfile = cond, priceperrow = PricePerRow.TICKSIZE);
 #mpro.Show(GlobalColor("Profile"), Color.Current, Color.CURRENT, 65);
@@ -24,7 +56,7 @@ def val = mpro.GetLowestValueArea();
 def vah = mpro.GetHighestValueArea();
 def pl = mpro.GetLowest();
 def ph = mpro.GetHighest();
-def vpoc = reference VolumeProfile(onexpansion = no, timeperprofile = "DAY", pricePerRowHeightMode = "TICKSIZE").POC;
+def vpoc = reference VolumeProfile(onexpansion = no, timeperprofile = timeperprofile, pricePerRowHeightMode = "TICKSIZE").POC;
 
 def ppc = if cond then pc[1] else if cond[-1] then Double.NaN else ppc[1];
 plot prev_pc = if ppc <> 0 then ppc else Double.NaN;
